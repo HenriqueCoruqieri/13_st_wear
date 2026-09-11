c05dbf06ac06f614a358d9e0e0e23d1caa0d2990
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-09-11 15:56:47 -0300

    chore: add prettier plugin tailwindcss setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="bg-red-500">
+    <div className="h-screen items-center bg-red-500">
       <h1 className="text-black">Olá mundo!!</h1>
     </div>
   );
